69ed96971bf84cfbc5753a9c29157434ec8a8619
jonathan <jonathancastro2018@yahoo.com>
2021-02-07 05:14:12 -0400

    carga del proeycto y avances

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\DataFixtures;
+
+// cargar la entidad
+use App\Entity\User;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+// agregamos para codificar contraseñas
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+
+	private $passwordEncoder;
+
+	public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+	{
+		$this->passwordEncoder = $passwordEncoder;
+	}
+
+	public function load(ObjectManager $manager)
+	{
+        // $product = new Product();
+        // $manager->persist($product);
+
+		$user = new User();
+
+		$user->setFirstName('jonathan');
+		$user->setLastName('castro');
+		$user->setEmail('admin@hotmail.com');
+		$user->setRoles(['ROLE_ADMIN']);	
+		$user->setPassword($this->passwordEncoder->encodePassword(
+			$user,
+			'123'
+		));
+
+		
+		$manager->persist($user);
+
+		$manager->flush();
+	}
+}
